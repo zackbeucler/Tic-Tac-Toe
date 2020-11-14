3dcd010c81955239a52bd6631c7552e1762546eb
bscully8 <70612146+bscully8@users.noreply.github.com>
2020-11-14 12:44:53 -0500

    Update test_TTT_unittest.py
    add test_input function

--- test_TTT_unittest.py
+++ test_TTT_unittest.py
@@ -7,7 +7,10 @@ class TestTicTacToe(unittest.TestCase):
     def test_winner(self):
 
     def test_input(self):
-
+        self.assertEqual(playerPickSymbol(first), "X")
+        self.assertEqual(playerPickSymbol(first), "x")
+        self.assertEqual(playerPickSymbol(first), "O")
+        self.assertEqual(playerPickSymbol(first), "o")
 
 if __name__ == '__main__':
     unittest.main()
