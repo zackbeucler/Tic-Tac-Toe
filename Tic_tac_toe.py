d38385dae87c4fdfdaab3a39673ae6a679149229
zackbeucler <zbeucler@conncoll.edu>
2020-11-14 13:09:11 -0500

    test_random

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -1,32 +1,40 @@
+# coding=utf-8
 import random
 
+
 def showGameRules():
-    #This function prints out the rules at the beginning of the game
+    # This function prints out the rules at the beginning of the game
     print("Tic Tac Toe!\n\n")
+    """
     print("RULES...................\n\n")
     print("1.  Two players select spaces on the 3×3 grid. The two players alternate when choosing spaces. (First turn should be selected randomly.)\n\n")
     print("2. If a player gets three in a row horizontally, vertically, or diagonally in any fashion, this player wins the game.\n\n")
     print("3. If the game continues until all the spaces are selected and neither player has three in a row, then the match is a draw/push.\n\n")
     print("To select a space, type one of the corresponding indices:\n\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n\n")
+    """
+
 
 def askPlayerName(i):
-    #This function asks for a player name and returns it
+    # This function asks for a player name and returns it
     return input("Player "+str(i)+" enter your name: ")
 
+
 def firstMove(player0, player1):
-    #This function takes the two players as parameters and chooses and returns the first player at random
-    if(random.randint(0,1) == 0):
+    # This function takes the two players as parameters and chooses and returns the first player at random
+    if(random.randint(0, 1) == 0):
         return player0
     else:
         return player1
 
+
 def playerPickSymbol(first):
-    #This function takes the first player as a parameter nd then returns the symbol they choose
+    # This function takes the first player as a parameter nd then returns the symbol they choose
     return input("\n"+first+", you go first! Type 'X' or 'O' to choose your symbol:  ")
-    
+
+
 def placePiece(player, i, symbols, board):
-    #This function takes the player, their symbol index, the list of symbols, and the board list as parameters
-    #The user chooses their space, and the function adds the selection to the board list and returns the board list
+    # This function takes the player, their symbol index, the list of symbols, and the board list as parameters
+    # The user chooses their space, and the function adds the selection to the board list and returns the board list
     taken = True
     while(taken):
         selection = eval(input("\n"+player+", make your selection: \n"))
@@ -34,86 +42,90 @@ def placePiece(player, i, symbols, board):
             taken = False
         else:
             print("\nThis space is taken. Try again: ")
-            
+
     board[selection-1] = symbols[i]
     return board
 
+
 def outputGameBoard(board):
-    #This function takes the board list as a parameter and prints out the current state of the game board
-    print("\n",board[0],"|",board[1],"|",board[2],"\n----------\n",board[3],"|",board[4],"|",board[5],"\n----------\n",board[6],"|",board[7],"|",board[8]+"\n")
+    # This function takes the board list as a parameter and prints out the current state of the game board
+    print("\n", board[0], "|", board[1], "|", board[2], "\n----------\n", board[3], "|",
+          board[4], "|", board[5], "\n----------\n", board[6], "|", board[7], "|", board[8]+"\n")
+
 
 def checkWinner(board, symbols, player0, player1):
-    #This function takes the board, symbols list, and both players as parameters, checks the game board to see if there is a winner,
-    #and outputs and returns the winner if there is one
-    if(board[1]!=" " and board[1] == board[0]):
+    # This function takes the board, symbols list, and both players as parameters, checks the game board to see if there is a winner,
+    # and outputs and returns the winner if there is one
+    if(board[1] != " " and board[1] == board[0]):
         if(board[2] == board[1]):
             if(board[1] == symbols[0]):
-                print(player0,"has won!")
+                print(player0, "has won!")
             else:
-                print(player1,"has won!")
+                print(player1, "has won!")
             return True
 
-    elif(board[5]!=" " and board[5] == board[2]):
+    elif(board[5] != " " and board[5] == board[2]):
         if(board[8] == board[5]):
             if(board[5] == symbols[0]):
-                print(player0,"has won!")
+                print(player0, "has won!")
             else:
-                print(player1,"has won!")
+                print(player1, "has won!")
             return True
-           
-    elif(board[4]!=" " and board[4] == board[1]):
+
+    elif(board[4] != " " and board[4] == board[1]):
         print("True")
         if(board[7] == board[4]):
             if(board[4] == symbols[0]):
-                print(player0,"has won!")
+                print(player0, "has won!")
             else:
-                print(player1,"has won!")
+                print(player1, "has won!")
             return True
-        
-    elif(board[4]!=" " and board[4] == board[3]):
+
+    elif(board[4] != " " and board[4] == board[3]):
         if(board[5] == board[4]):
             if(board[4] == symbols[0]):
-                print(player0,"has won!")
+                print(player0, "has won!")
             else:
-                print(player1,"has won!")
+                print(player1, "has won!")
             return True
-            
-    elif(board[7]!=" " and board[7] == board[6]):
+
+    elif(board[7] != " " and board[7] == board[6]):
         if(board[8] == board[7]):
             if(board[7] == symbols[0]):
-                print(player0,"has won!")
+                print(player0, "has won!")
             else:
-                print(player1,"has won!")
+                print(player1, "has won!")
             return True
-          
-    elif(board[3]!=" " and board[3] == board[0]):
+
+    elif(board[3] != " " and board[3] == board[0]):
         if(board[3] == board[6]):
             if(board[0] == symbols[0]):
-                print(player0,"has won!")
+                print(player0, "has won!")
             else:
-                print(player1,"has won!")
+                print(player1, "has won!")
             return True
-            
-    elif(board[4]!=" " and board[4] == board[0]):
+
+    elif(board[4] != " " and board[4] == board[0]):
         if(board[8] == board[4]):
             if(board[4] == symbols[0]):
-                print(player0,"has won!")
+                print(player0, "has won!")
             else:
-                print(player1,"has won!")
+                print(player1, "has won!")
             return True
-            
-    elif(board[4]!=" " and board[4] == board[2]):
+
+    elif(board[4] != " " and board[4] == board[2]):
         if(board[6] == board[4]):
             if(board[4] == symbols[0]):
-                print(player0,"has won!")
+                print(player0, "has won!")
             else:
-                print(player1,"has won!")
+                print(player1, "has won!")
             return True
 
     return False
 
+
 def fullBoard(board):
-    #This function takes the game board list as a parameter and returns a boolean for if the board is full or not
+    # This function takes the game board list as a parameter and returns a boolean for if the board is full or not
     count = 0
     for space in board:
         if(space == " "):
@@ -123,23 +135,25 @@ def fullBoard(board):
     if(count == 9):
         print("The board is full so the game ends in a tie.")
         return True
-    return False     
+    return False
+
 
 def playAgain():
-    #This function asks the user if they want to play again and returns their answer asn a boolean
+    # This function asks the user if they want to play again and returns their answer asn a boolean
     again = input("\nType 'p' to play again or type 'q' to quit: ")
     if(again.lower() == 'p'):
         return True
     return False
 
+
 def play_tic_tac_toe():
-    #main function from which all other functions are called
-    #runs through the tic tac toe game in proper order
+    # main function from which all other functions are called
+    # runs through the tic tac toe game in proper order
     again = True
 
     while(playAgain):
         showGameRules()
-        
+
         player0 = askPlayerName(0)
         print('\n')
         player1 = askPlayerName(1)
@@ -167,12 +181,12 @@ def play_tic_tac_toe():
                 symbols[0] = 'X'
                 symbols[1] = 'O'
 
-        board = [" "," "," "," "," "," "," "," "," "]
+        board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
         full = True
         winner = True
         turn = 0
-        while(winner and turn<9):
-            if(i%2==0):
+        while(winner and turn < 9):
+            if(i % 2 == 0):
                 board = placePiece(player0, 0, symbols, board)
                 current = player0
             else:
@@ -183,7 +197,7 @@ def play_tic_tac_toe():
 
             turn = turn + 1
             i = i + 1
-            
+
             if(checkWinner(board, symbols, player0, player1)):
                 winner = False
             if(fullBoard(board)):
@@ -191,10 +205,6 @@ def play_tic_tac_toe():
 
         if(playAgain() is not True):
             again = False
-        
-play_tic_tac_toe() #starts a game of tic tac toe
-        
-    
-        
-        
 
+
+play_tic_tac_toe()  # starts a game of tic tac toe
--- test_TTT_unittest.py
+++ test_TTT_unittest.py
@@ -1,12 +1,22 @@
+import Tic_tac_toe
 import unittest
+
 
 class TestTicTacToe(unittest.TestCase):
 
     def test_random(self):
+        for i in range(100):
+            counter = 0
+            if (Tic_tac_toe.firstMove("player0", "player1") == "player1"):
+                counter += 1
+
+        self.assertEqual(counter, 50)
 
     def test_winner(self):
+        print()
 
     def test_input(self):
+        print()
 
 
 if __name__ == '__main__':
