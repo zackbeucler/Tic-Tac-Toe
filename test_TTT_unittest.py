6de3a961db5b9b8240ef3eca29d493ba016b3611
jturbeville <49793028+jturbeville@users.noreply.github.com>
2020-11-14 12:27:34 -0500

    adding test class

---
+++ test_TTT_unittest.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestTicTacToe(unittest.TestCase):
+
+    def test_random(self):
+
+    def test_winner(self):
+
+    def test_input(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
