40eda627bf9efe166e032c4a48b2017e59578cde
jturbeville <49793028+jturbeville@users.noreply.github.com>
2020-12-06 12:40:43 -0500

    update for a10 grading

--- game-log.py
+++ game-log.py
@@ -3,8 +3,9 @@ import logging.handlers
 import os
 from datetime import datetime
 import sys
- 
+
 log = logging.getLogger("script-logger")
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
 formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", "game-log.log"))
