e68aeca8b045eb386dcead7842880ba95adf511b
jturbeville <49793028+jturbeville@users.noreply.github.com>
2020-12-15 22:44:38 -0500

    final edits before presentation

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -5,19 +5,16 @@ from game_log import log
 def showGameRules():
     # This function prints out the rules at the beginning of the game
     print("Tic Tac Toe!\n\n")
-    
     print("RULES...................\n\n")
     print("1.  Two players select spaces on the 3×3 grid. The two players alternate when choosing spaces. (First turn should be selected randomly.)\n\n")
     print("2. If a player gets three in a row horizontally, vertically, or diagonally in any fashion, this player wins the game.\n\n")
     print("3. If the game continues until all the spaces are selected and neither player has three in a row, then the match is a draw/push.\n\n")
-    print("To select a space, type one of the corresponding indices:\n\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n\n")
-    
-
+    print("To select a space, type one of the corresponding indices:\n\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n")
 
 def askPlayerName(i):
     # This function asks for a player name and returns it
     # only error is if the player doesent enter a name at all
-    playerName = input("Player "+str(i)+" enter your name: ")
+    playerName = input("\nPlayer "+str(i)+" enter your name: ")
     while playerName == "":
         print("Invalid name entered")
         playerName = input("Player "+str(i)+" enter your name: ")
@@ -54,40 +51,33 @@ def placePiece(player, i, symbols, board):
     # The user chooses their space, and the function adds the selection to the board list and returns the board list
 
     #error handling
-    validNums = [1,2,3,4,5,6,7,8,9]
-    taken = True
     valid = False
-
+    
     while(not valid):
-        valid = True
-        selection = input("\nPlease enter the index of the space you would like to select: ")
+        taken = True
         try:
-            selection = int(selection)
-        except(ValueError):
-            print("\nPlease enter an integer between 1 and 9 for the sqpace you would like to choose. Make sure this space has not been taken already")
-            valid = False
+            selection = int(input("\nPlease enter the index of the space you would like to select: "))
+            if(board[selection-1] != " "):
+                print("\nSorry! This space is already taken. Please take another look at the board and pick a different space.")
+                outputGameBoard(board)
+            else:
+                board[selection-1] = symbols[i]
+                taken = False
+        except(ValueError, IndexError):
+            print("\nSorry! The integer you have entered is not valid or the space is already taken. Please"+ 
+            " make sure you are entering an integer between 1 and 9.")
+            outputGameBoard(board)
         
-    while(taken):
-        
-        if(selection not in validNums):   #handles error --> player enters invalid int
-            print("\nPlease enter an integer between 1 and 9 for the sqpace you would like to choose. Make sure this space has not been taken already")
-        elif(board[selection-1] == " "):
-            taken = False
-            valid = False
-        else:   #handles --> player selects a space that is taken
-            print("\nThis space is taken. Try again: ")
+        if(taken == False):
+            valid = True
         log.info("selection: "+str(selection))
 
-
-
-    board[selection-1] = symbols[i]
     return board
-
 
 def outputGameBoard(board):
     # This function takes the board list as a parameter and prints out the current state of the game board
     print("\n", board[0], "|", board[1], "|", board[2], "\n----------\n", board[3], "|",
-          board[4], "|", board[5], "\n----------\n", board[6], "|", board[7], "|", board[8]+"\n")
+          board[4], "|", board[5], "\n----------\n", board[6], "|", board[7], "|", board[8])
 
 
 def checkWinner(board, symbols, player0, player1):
@@ -96,66 +86,66 @@ def checkWinner(board, symbols, player0, player1):
     if(board[1] != " " and board[1] == board[0]):
         if(board[2] == board[1]):
             if(board[1] == symbols[0]):
-                print(player0, "has won!")
+                print("\n"+player0, "has won!")
             else:
-                print(player1, "has won!")
+                print("\n"+player1, "has won!")
             return True
 
     if(board[5] != " " and board[5] == board[2]):
         if(board[8] == board[5]):
             if(board[5] == symbols[0]):
-                print(player0, "has won!")
+                print("\n"+player0, "has won!")
             else:
-                print(player1, "has won!")
+                print("\n"+player1, "has won!")
             return True
 
     if(board[4] != " " and board[4] == board[1]):
         print("True")
         if(board[7] == board[4]):
             if(board[4] == symbols[0]):
-                print(player0, "has won!")
+                print("\n"+player0, "has won!")
             else:
-                print(player1, "has won!")
+                print("\n"+player1, "has won!")
             return True
 
     if(board[4] != " " and board[4] == board[3]):
         if(board[5] == board[4]):
             if(board[4] == symbols[0]):
-                print(player0, "has won!")
+                print("\n"+player0, "has won!")
             else:
-                print(player1, "has won!")
+                print("\n"+player1, "has won!")
             return True
 
     if(board[7] != " " and board[7] == board[6]):
         if(board[8] == board[7]):
             if(board[7] == symbols[0]):
-                print(player0, "has won!")
+                print("\n"+player0, "has won!")
             else:
-                print(player1, "has won!")
+                print("\n"+player1, "has won!")
             return True
 
     if(board[3] != " " and board[3] == board[0]):
         if(board[3] == board[6]):
             if(board[0] == symbols[0]):
-                print(player0, "has won!")
+                print("\n"+player0, "has won!")
             else:
-                print(player1, "has won!")
+                print("\n"+player1, "has won!")
             return True
 
     if(board[4] != " " and board[4] == board[0]):
         if(board[8] == board[4]):
             if(board[4] == symbols[0]):
-                print(player0, "has won!")
+                print("\n"+player0, "has won!")
             else:
-                print(player1, "has won!")
+                print("\n"+player1, "has won!")
             return True
 
     if(board[4] != " " and board[4] == board[2]):
         if(board[6] == board[4]):
             if(board[4] == symbols[0]):
-                print(player0, "has won!")
+                print("\n"+player0, "has won!")
             else:
-                print(player1, "has won!")
+                print("\n"+player1, "has won!")
             return True
 
     return False
@@ -170,7 +160,7 @@ def fullBoard(board):
         else:
             count += 1
     if(count == 9):
-        print("The board is full so the game ends in a tie.")
+        print("\nThe board is full so the game ends in a tie.")
         return True
     return False
 
@@ -188,7 +178,7 @@ def playAgain():
             if(again.lower() == 'p'):
                 return True
             else:
-                print("Thanks for playing!")
+                print("\nThanks for playing!")
                 valid = False
     return False
 
@@ -202,7 +192,6 @@ def play_tic_tac_toe():
         showGameRules()
 
         player0 = askPlayerName(0)
-        print('\n')
         player1 = askPlayerName(1)
 
         i = 3
--- game_log.py
+++ game_log.py
@@ -2,20 +2,25 @@ import logging
 import logging.handlers
 import os
 from datetime import datetime
-import sys
+# import sys
+
+# log = logging.getLogger("script-logger")
+# logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
+# formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+# handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", "game-log.log"))
+
+# handler.setFormatter(formatter)
+# sys_handler = logging.StreamHandler(stream=sys.stdout)
+# sys_handler.setFormatter(formatter)
+
+# log.setLevel(logging.INFO)
+# log.addHandler(handler)
+# log.addHandler(sys_handler)
 
 log = logging.getLogger("script-logger")
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-
-formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", "game-log.log"))
-
-handler.setFormatter(formatter)
-sys_handler = logging.StreamHandler(stream=sys.stdout)
-sys_handler.setFormatter(formatter)
-
 log.setLevel(logging.INFO)
+handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", "game_log.log"))
+formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s","%Y:%m:%d:%H:%M:%S")
+handler.setFormatter(formatter)
 log.addHandler(handler)
-log.addHandler(sys_handler)
-
-
